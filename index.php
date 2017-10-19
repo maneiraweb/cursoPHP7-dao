9f02ee5c3e379184028221b573388dd7f8db4345
maneiraweb <ricardo@maneiraweb.com.br>
2017-10-18 23:44:57 -0200

    Busca por login e senha

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
 require_once("config.php");
-
-/*$sql = new sql();
-
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($usuarios);
-
-*/
-
-$root = new Usuario();
-
-$root->loadbyId(4);
-
-echo $root;
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("ri");
+//echo json_encode($search);
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("ricardo", "1234567890");
+//echo $usuario;
 ?>
 
