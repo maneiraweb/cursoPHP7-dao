dd80c9a7dcd2da80a098ff934dbdf61b73cbee3b
maneiraweb <ricardo@maneiraweb.com.br>
2017-10-18 22:55:58 -0200

    criamos a classe usuarios

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 
 require_once("config.php");
 
-$sql = new sql();
+/*$sql = new sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 
+*/
+
+$root = new Usuario();
+
+$root->loadbyId(4);
+
+echo $root;
 ?>
+
